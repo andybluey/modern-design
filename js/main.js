c2119246b9be56f7baf94fe1ac6365d80c5472ae
Andrew Knight <andrewjaknight@gmail.com>
2016-11-14 09:57:07 +1100

    Added transitions to background images and some formatting goodness

--- js/main.js
+++ js/main.js
@@ -8,6 +8,26 @@ $(document).ready(function(){
 	// Animate margin of Signup button
 	$("#signUp").animate({
 	  marginRight: "0px"
-	}, 1000);
+	}, 1500);
+
+	// Transform the background image
+  // For the intro page
+  $(window).scroll(function() {
+  var scrollTop = $(window).scrollTop();
+  console.log(scrollTop);
+  var limit = 700;
+    $("div.landing-page").css({
+        transform: "translate(0%, " + ((scrollTop/limit)*20) + "%) translate3d(0px, 0px, 0px)"
+    });
+  });
+  // For the about page
+  $(window).scroll(function() {
+  var scrollTop = $(window).scrollTop();
+  console.log(scrollTop);
+  var limit = 700;
+    $("div.about-page").css({
+        transform: "translate(0%, " + ((scrollTop/limit)*10) + "%) translate3d(0px, 0px, 0px)"
+    });
+  });
 
 });
